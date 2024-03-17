d359e9e4b11d66562036f4f7cd15ca492b4e3dfb
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2024-03-17 00:16:29 -0500

    Add database annotations to Product class

--- server/src/main/java/com/sawolecommerce/ecommerce/Product.java
+++ server/src/main/java/com/sawolecommerce/ecommerce/Product.java
@@ -1,6 +1,19 @@
 package com.sawolecommerce.ecommerce;
 
+// Information on how to use database in spring boot: https://spring.io/guides/gs/accessing-data-mysql
+
+// Importing the necessary libraries
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+
 public class Product implements java.io.Serializable{ // Serializable is used to convert the object into a byte stream so that it can be saved to a file, sent over the network, or stored in a database. It is a marker interface, which means it has no methods.
+
+    @Id // This tells Hibernate to make a column in the table as the primary key
+    @GeneratedValue(strategy=GenerationType.AUTO) // This tells Hibernate to generate the value automatically
     private String name;
     private String description;
     private double price;
