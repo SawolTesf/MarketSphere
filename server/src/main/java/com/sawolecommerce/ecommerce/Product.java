aecc5be6740e95f624aad89e4ea754306df9d501
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2024-03-16 23:23:31 -0500

    Add setters and toString method to Product class

--- server/src/main/java/com/sawolecommerce/ecommerce/Product.java
+++ server/src/main/java/com/sawolecommerce/ecommerce/Product.java
@@ -36,4 +36,33 @@ public class Product implements java.io.Serializable{ // Serializable is used to
     public String getImageUrl() {
         return imageUrl;
     }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+      return "Product{" +
+              "name='" + name + '\'' +
+              ", description='" + description + '\'' +
+              ", price=" + price +
+              ", category='" + category + '\'' +
+              ", imageUrl='" + imageUrl + '\'' +
+              '}';
+    }
 }
