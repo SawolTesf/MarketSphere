da2b28d8e14817140fc1646b685226eb490a1961
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2024-03-22 23:39:39 -0500

    Add addProduct method to ProductService

--- server/src/main/java/com/sawolecommerce/service/ProductService.java
+++ server/src/main/java/com/sawolecommerce/service/ProductService.java
@@ -3,6 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sawolecommerce.repo.ProductRepo;
+import com.sawolecommerce.model.Product;
 
 @Service // This tells Spring that this class is a service which in simple terms is a class that contains business logic. Business logic is the part of the program that encodes the real-world business rules that determine how data can be created, stored, and changed.
 public class ProductService {
@@ -14,4 +15,8 @@ public class ProductService {
   }
 
   // Methods
+  public Product addProduct(Product product){
+    return productRepo.save(product); // This method is used to save an entity to the database
+  }
+
 }
