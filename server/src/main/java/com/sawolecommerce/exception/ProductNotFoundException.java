c1946a542a5ca52aaebe2d465e2191dc7a740af6
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2024-04-05 22:40:02 -0500

    Add ProductNotFoundException class to handle product not found exception and implement it

---
+++ server/src/main/java/com/sawolecommerce/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sawolecommerce.exception;
+
+public class ProductNotFoundException extends RuntimeException{
+    public ProductNotFoundException(String message){
+        super(message); // This calls the constructor of the parent class (RuntimeException) and passes the message to it. Shows how we can use polymorphism and inheritance in Java
+    }
+}
--- server/src/main/java/com/sawolecommerce/service/ProductService.java
+++ server/src/main/java/com/sawolecommerce/service/ProductService.java
@@ -1,18 +1,18 @@
 /* The point of the service layer is to hold the business logic of the application. It calls methods from the repository layer to interact with the database. Here is where you would handle things like transactions and security */
 package com.sawolecommerce.service;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sawolecommerce.repo.ProductRepo;
 import com.sawolecommerce.model.Product;
-
+import com.sawolecommerce.exception.ProductNotFoundException;
 import java.util.List;
 
 @Service // This tells Spring that this class is a service which in simple terms is a class that contains business logic. Business logic is the part of the program that encodes the real-world business rules that determine how data can be created, stored, and changed.
 public class ProductService {
   private final ProductRepo productRepo;
 
-  @Autowired // This annotation allows Spring to resolve and inject collaborating beans into your bean which means that it will automatically create an instance of ProductRepo and inject it into ProductService
+//  @Autowired // This annotation allows Spring to resolve and inject collaborating beans into your bean which means that it will automatically create an instance of ProductRepo and inject it into ProductService
   public ProductService(ProductRepo productRepo){
     this.productRepo = productRepo;
   }
@@ -31,7 +31,7 @@ public class ProductService {
   }
 
   public Product findProductById(Long productId){
-    return productRepo.findById(productId).orElseThrow(() -> new RuntimeException("Product by id " + productId + " was not found")); // This method is used to retrieve an entity from the database by its id
+    return productRepo.findById(productId).orElseThrow(() -> new ProductNotFoundException("Product by id " + productId + " was not found")); // This method is used to retrieve an entity from the database by its id and if it is not found, it throws a ProductNotFoundException which was created in the exception package
   }
 
   public void deleteProduct(Long productId){
