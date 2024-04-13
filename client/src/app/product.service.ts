43fc7d148d758a921a148bf2401371bb77323eea
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2024-04-13 00:02:19 -0500

    Add ProductService class with HttpClient dependency and apiServerUrl property

--- client/src/app/product.service.ts
+++ client/src/app/product.service.ts
@@ -2,3 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from './product';
+
+// This means that the service will be provided in the root module, which is the AppModule. 
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService{
+  private apiServerUrl = ''; // This is blank because we are using a proxy to connect to the backend server. A proxy is what sits between the client and the server which is used to forward requests to the server.
+
+  constructor(private http: HttpClient){} // Injecting the HttpClient service into the ProductService class.
+
+
+}
