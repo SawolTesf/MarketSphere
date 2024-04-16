a81344a85f06c260323efc14b24164dd2bef5f10
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2024-04-15 22:47:08 -0500

    Add OnInit to call getProduct()

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import { ProductService } from './product.service';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -10,10 +9,15 @@ import { HttpErrorResponse } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   public products: Product[] = [];
 
   constructor(private productService: ProductService){}
+
+  // This method is called when the component is initialized.
+  ngOnInit(){
+    this.getProduct();
+  }
 
   public getProduct(): void{
     this.productService.getProduct().subscribe({
