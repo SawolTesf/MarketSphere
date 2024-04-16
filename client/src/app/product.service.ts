d1d52b9abdfe59965479fa3d5f549a2c2d5694f4
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2024-04-15 22:58:36 -0500

    Update apiServerUrl in ProductService to use environment variable

--- client/src/app/product.service.ts
+++ client/src/app/product.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'; // rxjs is a library for reactive programming using Observables, to make it easier to compose asynchronous or callback-based code.
 import { Product } from './product';
+import { environment } from '../environments/environment';
 
 // This means that the service will be provided in the root module, which is the AppModule. 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService{
-  private apiServerUrl = ''; // This is blank because we are using a proxy to connect to the backend server. A proxy is what sits between the client and the server which is used to forward requests to the server.
+  private apiServerUrl = environment.apiUrl; // This is blank because we are using a proxy to connect to the backend server. A proxy is what sits between the client and the server which is used to forward requests to the server.
 
   constructor(private http: HttpClient){} // Injecting the HttpClient service into the ProductService class.
 
