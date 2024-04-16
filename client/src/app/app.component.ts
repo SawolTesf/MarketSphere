e4caf473a7f1719e82938a97224f4221a52d2b71
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2024-04-15 22:33:49 -0500

    Add getProduct method to AppComponent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Product } from './product';
+import { ProductService } from './product.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
+  public products: Product[] = [];
+
+  constructor(private productService: ProductService){}
+
+  public getProduct(): void{
+    this.productService.getProduct().subscribe({
+      next: (response: Product[]) => { this.products = response; }, // when the response is received, the products array is updated with the response.
+      error: (error: HttpErrorResponse) => { alert(error.message); },
+      complete: () => { console.log('Completed'); }
+  });
+  }
 }
