ae7e0be5a10c94bf0662e8b22c480b94eca89aed
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2024-04-19 22:06:16 -0500

    Add AppModule to the Angular application

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { ProductService } from './product.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    HttpClientModule
+  ],
+  providers: [ProductService, HttpClientModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
